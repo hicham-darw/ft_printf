b45d8e374b277ea866a08add60054f24d6d20e33
hicham-darw <hamoudhicham234@gmail.com>
2025-08-31 17:34:58 +0100

    FOR TEST YOUR libftprintf

--- main.c
+++ main.c
@@ -1,72 +1,58 @@
 /***************************************************************************************
-
-|***libft.h in my libftprintf.h should called "libft/" and in the root of repository***|
-
+|										       |
+|   	    THIS MAIN TEST ONLY MANDATORY PART - BONUS PART IS EXECLUDED               |
+|   libft.h in my libftprintf.h should called "libft/" in the root of your repository  |
+|										       |
 ***************************************************************************************/
 #include "libftprintf.h"
 #include <stdio.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
 
-// Your ft_printf prototype
-int ft_printf(const char *format, ...);
+#define TEST_COUNT 1000
+#define STR_COUNT 5
 
 int main(void)
 {
-    int ret_ft, ret_std;
-    int failures = 0;
-    int i;
+    int ret_std, ret_ft;
+    srand(time(NULL));
 
-    char *strings[] = { "Hello", "", NULL, "StressTest123", "LongStringForTesting" };
-    int ints[] = { 0, 1, -1, 42, INT_MAX, INT_MIN };
-    unsigned int uints[] = { 0, 1, 42, UINT_MAX };
-    char chars[] = { 'a', 'Z', '\0' };
+    const char *samples[STR_COUNT] = {"", "hello", "world", "ft_printf", NULL};
 
-    void *pointers[] = { (void*)0, (void*)1, (void*)ints, (void*)&ints[0] };
+    printf("===== SUPER MULTI-STRING STRESS TEST =====\n\n");
 
-    for (i = 0; i < 1000; i++)
-    {
-        // Integer
-        ret_ft = ft_printf("Int: %d\n", ints[i % 6]);
-        ret_std = printf("Int: %d\n", ints[i % 6]);
-        if (ret_ft != ret_std) { printf("FAIL #%d: ret_ft=%d | ret_std=%d\n", i+1, ret_ft, ret_std); failures++; }
+    for (int i = 0; i < TEST_COUNT; i++) {
+        int di = rand() % (INT_MAX / 2) - (INT_MAX / 4);
+        unsigned int u = rand();
+        char c = (char)(rand() % 128);
+        const char *s1 = samples[rand() % STR_COUNT];
+        const char *s2 = samples[rand() % STR_COUNT];
+        const char *s3 = samples[rand() % STR_COUNT];
+        void *p = (i % 10 == 0) ? NULL : (void *)(uintptr_t)(rand() % 0xFFFFF);
+        unsigned int x = rand();
+        unsigned int X = rand();
 
-        // Unsigned
-        ret_ft = ft_printf("Unsigned: %u\n", uints[i % 4]);
-        ret_std = printf("Unsigned: %u\n", uints[i % 4]);
-        if (ret_ft != ret_std) { printf("FAIL #%d: ret_ft=%d | ret_std=%d\n", i+1, ret_ft, ret_std); failures++; }
+        ret_std = printf(
+            "std %%d %%u %%c %%s %%s %%s %%p %%x %%X: |%d|%u|%c|%s|%s|%s|%p|%x|%X|\n",
+            di, u, c,
+            s1 ? s1 : "(null)",
+            s2 ? s2 : "(null)",
+            s3 ? s3 : "(null)",
+            p, x, X
+        );
 
-        // Hex
-        ret_ft = ft_printf("Hex lowercase: %x\n", uints[i % 4]);
-        ret_std = printf("Hex lowercase: %x\n", uints[i % 4]);
-        if (ret_ft != ret_std) { printf("FAIL #%d: ret_ft=%d | ret_std=%d\n", i+1, ret_ft, ret_std); failures++; }
+        ret_ft = ft_printf(
+            "ft  %%d %%u %%c %%s %%s %%s %%p %%x %%X: |%d|%u|%c|%s|%s|%s|%p|%x|%X|\n",
+            di, u, c, s1, s2, s3, p, x, X
+        );
 
-        ret_ft = ft_printf("Hex uppercase: %X\n", uints[i % 4]);
-        ret_std = printf("Hex uppercase: %X\n", uints[i % 4]);
-        if (ret_ft != ret_std) { printf("FAIL #%d: ret_ft=%d | ret_std=%d\n", i+1, ret_ft, ret_std); failures++; }
-
-        // Char
-        ret_ft = ft_printf("Char: %c\n", chars[i % 3]);
-        ret_std = printf("Char: %c\n", chars[i % 3]);
-        if (ret_ft != ret_std) { printf("FAIL #%d: ret_ft=%d | ret_std=%d\n", i+1, ret_ft, ret_std); failures++; }
-
-        // String
-        ret_ft = ft_printf("String: %s\n", strings[i % 5]);
-        ret_std = printf("String: %s\n", strings[i % 5]);
-        if (ret_ft != ret_std) { printf("FAIL #%d: ret_ft=%d | ret_std=%d\n", i+1, ret_ft, ret_std); failures++; }
-
-        // Pointer
-        ret_ft = ft_printf("Pointer: %p\n", pointers[i % 4]);
-        ret_std = printf("Pointer: %p\n", pointers[i % 4]);
-        if (ret_ft != ret_std) { printf("FAIL #%d: ret_ft=%d | ret_std=%d\n", i+1, ret_ft, ret_std); failures++; }
-
-        // Percent
-        ret_ft = ft_printf("Percent: %%\n");
-        ret_std = printf("Percent: %%\n");
-        if (ret_ft != ret_std) { printf("FAIL #%d: ret_ft=%d | ret_std=%d\n", i+1, ret_ft, ret_std); failures++; }
+        if (ret_std != ret_ft)
+            printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! RET MISMATCH at iteration %d !!!\n", i);
     }
 
-    printf("Total tests: %d | Failures: %d\n", 1000*8, failures);
+    printf("===== MULTI-STRING STRESS TEST COMPLETE =====\n");
     return 0;
 }
 
