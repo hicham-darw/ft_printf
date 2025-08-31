bce8f8ee70b6280f96dca6f167f2bb67fd0a383f
hicham-darw <hamoudhicham234@gmail.com>
2025-08-31 14:52:03 +0100

    some changes to fix function

--- ft_printf.c
+++ ft_printf.c
@@ -1,30 +1,42 @@
 #include "libftprintf.h"
 
+static char	*get_string(char *dest, const char *frmt, va_list list, int *len)
+{
+	char	*p_start;
+
+	if (!frmt)
+		return (dest);
+	while (*frmt)
+	{
+		p_start = (char *)frmt;
+		while (*frmt && *frmt != '%')
+			frmt++;
+		dest = ft_strncat(dest, p_start, frmt - p_start);
+		if (*(frmt) == '%')
+		{
+			dest = ft_strcat_format(dest, *(frmt + 1), list, len);
+			frmt += 2;
+		}
+	};
+	*len += ft_strlen(dest);
+	return (dest);
+}
+
 int	ft_printf(const char *frmt, ...)
 {
 	va_list	list;
 	char	*tmp;
-	int	len;
-	char	*p_start;
-	
+	int		len;
+
 	va_start(list, frmt);
 	len = ft_count_len(frmt, list);
 	tmp = (char *)ft_memalloc(sizeof(char) * (len + 1));
 	if (!tmp)
 		return (-1);
-	while (*frmt)
-	{
-		p_start = (char *)frmt;
-		while(*frmt && *frmt != '%')
-			frmt++;
-		tmp = ft_strncat(tmp, (frmt  - (frmt - p_start)), frmt - p_start);
-		if (*(frmt) == '%')
-		{
-			tmp = ft_strcat_format(tmp, *(frmt + 1),list);
-			frmt += 2;
-		}
-	}
+	len = 0;
+	get_string(tmp, frmt, list, &len);
 	ft_putstr(tmp);
+	free(tmp);
 	va_end(list);
 	return (len);
 }
